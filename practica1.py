2b6eceb2f3e4b02acf2a6cf2445ec2a96799dda1
nicolasgarcia1 <104448339+nicolasgarcia1@users.noreply.github.com>
2022-05-11 20:40:20 -0300

    Add files via upload
    0.1

---
+++ practica1.py
@@ -0,0 +1,18 @@
+import os, time 
+"""
+informe sobre canasta basica
+Nicolás García ISI 107
+"""
+
+def clear():
+    if os.name == "nt":
+        return os.system("cls")
+
+def ingreso_supermercados():
+    print("Ingrese los nombres de los supermercados")
+    option = input(int())
+    while option != "*":
+        
+
+    
+    
